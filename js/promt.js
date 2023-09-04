f1f4992badd5c9618f375369b8d321674918cfd8
naeem <naeemislamjr182@gmail.com>
2023-09-04 23:02:54 +0600

    aleart confirmn prompt

--- js/promt.js
+++ js/promt.js
@@ -1,1 +1,20 @@
 console.log('naeem')
+
+const alertUser = () => {
+    alert("more alert e chapsos")
+}
+const seriousStudent = () => {
+    const serious = confirm('Are you spending 6/8 hour eery single day for dev?')
+
+    if (serious === true) {
+        alert('Well done! keep it up.')
+    }
+    else {
+        alert('Valo hoye jao!!!! kopale dukkho ase!')
+    }
+}
+
+const getName = () => {
+    const name = prompt('What is your name?')
+    console.log(name)
+}
